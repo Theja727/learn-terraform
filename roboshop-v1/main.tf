d00f382346fee27c7dbec49e63e3b57e343b45d4
Teja <awstheja727@gmail.com>
2024-12-30 12:19:35 +0530

    Learning terraform - Creating EC2 instances and Route53 records

--- roboshop-v1/main.tf
+++ roboshop-v1/main.tf
@@ -9,4 +9,186 @@ terraform {
 resource "aws_instance" "frontend" {
   ami="ami-09c813fb71547fc4f"
   instance_type = "t2.micro"
+  vpc_security_group_ids = ["sg-0049ef5ba290b1d61"]
+  tags = {
+    Name = "frontend"
+  }
+}
+
+resource "aws_route53_record" "frontend" {
+  zone_id = Z06372633BKYPXSU4MUBQ
+  name    = "frontend-dev.tejadevopsb81.icu"
+  type    = "A"
+  ttl     = 300
+  records = [aws_instance.frontend.private_ip]
+}
+
+resource "aws_instance" "mongodb" {
+  ami="ami-09c813fb71547fc4f"
+  instance_type = "t2.micro"
+  vpc_security_group_ids = ["sg-0049ef5ba290b1d61"]
+  tags = {
+    Name = "mongodb"
+  }
+}
+
+resource "aws_route53_record" "mongodb" {
+  zone_id = Z06372633BKYPXSU4MUBQ
+  name    = "mongodb-dev.tejadevopsb81.icu"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.mongodb.private_ip]
+}
+
+resource "aws_instance" "catalogue" {
+  ami="ami-09c813fb71547fc4f"
+  instance_type = "t2.micro"
+  vpc_security_group_ids = ["sg-0049ef5ba290b1d61"]
+  tags = {
+    Name = "catalogue"
+  }
+}
+
+resource "aws_route53_record" "catalogue" {
+  zone_id = Z06372633BKYPXSU4MUBQ
+  name    = "catalogue-dev.tejadevopsb81.icu"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.catalogue.private_ip]
+}
+
+resource "aws_instance" "redis" {
+  ami="ami-09c813fb71547fc4f"
+  instance_type = "t2.micro"
+  vpc_security_group_ids = ["sg-0049ef5ba290b1d61"]
+  tags = {
+    Name = "redis"
+  }
+}
+
+resource "aws_route53_record" "redis" {
+  zone_id = Z06372633BKYPXSU4MUBQ
+  name    = "redis-dev.tejadevopsb81.icu"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.redis.private_ip]
+}
+
+resource "aws_instance" "user" {
+  ami="ami-09c813fb71547fc4f"
+  instance_type = "t2.micro"
+  vpc_security_group_ids = ["sg-0049ef5ba290b1d61"]
+  tags = {
+    Name = "user"
+  }
+}
+
+resource "aws_route53_record" "user" {
+  zone_id = Z06372633BKYPXSU4MUBQ
+  name    = "user-dev.tejadevopsb81.icu"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.user.private_ip]
+}
+
+resource "aws_instance" "cart" {
+  ami="ami-09c813fb71547fc4f"
+  instance_type = "t2.micro"
+  vpc_security_group_ids = ["sg-0049ef5ba290b1d61"]
+  tags = {
+    Name = "cart"
+  }
+}
+
+resource "aws_route53_record" "cart" {
+  zone_id = Z06372633BKYPXSU4MUBQ
+  name    = "cart-dev.tejadevopsb81.icu"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.cart.private_ip]
+}
+
+resource "aws_instance" "mysql" {
+  ami="ami-09c813fb71547fc4f"
+  instance_type = "t2.micro"
+  vpc_security_group_ids = ["sg-0049ef5ba290b1d61"]
+  tags = {
+    Name = "mysql"
+  }
+}
+
+resource "aws_route53_record" "mysql" {
+  zone_id = Z06372633BKYPXSU4MUBQ
+  name    = "mysql-dev.tejadevopsb81.icu"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.mysql.private_ip]
+}
+
+resource "aws_instance" "shipping" {
+  ami="ami-09c813fb71547fc4f"
+  instance_type = "t2.micro"
+  vpc_security_group_ids = ["sg-0049ef5ba290b1d61"]
+  tags = {
+    Name = "shipping"
+  }
+}
+
+resource "aws_route53_record" "shipping" {
+  zone_id = Z06372633BKYPXSU4MUBQ
+  name    = "shipping-dev.tejadevopsb81.icu"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.shipping.private_ip]
+}
+
+resource "aws_instance" "rabbitmq" {
+  ami="ami-09c813fb71547fc4f"
+  instance_type = "t2.micro"
+  vpc_security_group_ids = ["sg-0049ef5ba290b1d61"]
+  tags = {
+    Name = "rabbitmq"
+  }
+}
+
+resource "aws_route53_record" "rabbitmq" {
+  zone_id = Z06372633BKYPXSU4MUBQ
+  name    = "rabbitmq-dev.tejadevopsb81.icu"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.rabbitmq.private_ip]
+}
+
+resource "aws_instance" "payment" {
+  ami="ami-09c813fb71547fc4f"
+  instance_type = "t2.micro"
+  vpc_security_group_ids = ["sg-0049ef5ba290b1d61"]
+  tags = {
+    Name = "payment"
+  }
+}
+
+resource "aws_route53_record" "payment" {
+  zone_id = Z06372633BKYPXSU4MUBQ
+  name    = "payment-dev.tejadevopsb81.icu"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.payment.private_ip]
+}
+
+resource "aws_instance" "dispatch" {
+  ami="ami-09c813fb71547fc4f"
+  instance_type = "t2.micro"
+  vpc_security_group_ids = ["sg-0049ef5ba290b1d61"]
+  tags = {
+    Name = "dispatch"
+  }
+}
+
+resource "aws_route53_record" "dispatch" {
+  zone_id = Z06372633BKYPXSU4MUBQ
+  name    = "dispatch-dev.tejadevopsb81.icu"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.dispatch.private_ip]
 }
