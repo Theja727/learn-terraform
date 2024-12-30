c4dbb9d4ee9c651790d8217a034484a87824cf95
Teja <awstheja727@gmail.com>
2024-12-30 15:52:06 +0530

    Learning terraform - variables

--- 02-variables/main.tf
+++ 02-variables/main.tf
@@ -9,3 +9,28 @@ output "URL" {
 output "URL1" {
   value = "URL - ${var.URL}"
 }
+
+variable "list1" {
+  default = [10,20,"str",false]
+}
+
+variable "map1" {
+  default = {
+    aws = {
+      trainer = "John"
+      duration = 30
+    }
+    devops = {
+      trainer = "Steve"
+      duration = 20
+    }
+  }
+}
+
+output "list1_1" {
+  value = var.list1[3]
+}
+
+output "map1_1" {
+  value = var.map1["aws"]["trainer"]
+}
