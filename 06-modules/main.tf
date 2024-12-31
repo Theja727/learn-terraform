9624ef04458228075c987c2c740a0a72060db102
Teja <awstheja727@gmail.com>
2024-12-31 09:15:50 +0530

    Learning terraform - roboshop-modules-ec2-route53

---
+++ 06-modules/main.tf
@@ -0,0 +1,11 @@
+variable "instances" {
+  default = ["test1", "test2"]
+}
+
+module "ec2" {
+  count = length(var.instances)
+  source = "./ec2"
+  instance_name = var.instances[count.index]
+}
+
+variable "instance_name" {}
