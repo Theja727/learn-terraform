5d2bade52d7bae268a3ce17e43043a0295ad3b45
Teja <awstheja727@gmail.com>
2024-12-31 08:30:45 +0530

    Learning terraform - roboshop module

--- roboshop-module/module/main.tf
+++ roboshop-module/module/main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "instance" {
   ami           = "ami-09c813fb71547fc4f"
-  instance_type = "t3.small"
-  vpc_security_group_ids = ["sg-0be95cb0c3fef9448"]
+  instance_type = "t2.micro"
+  vpc_security_group_ids = ["sg-0049ef5ba290b1d61"]
   tags = {
     Name = var.component_name
   }
@@ -9,7 +9,7 @@ resource "aws_instance" "instance" {
 
 resource "aws_route53_record" "record" {
   zone_id = "Z02249652EM5BAO495DZ1"
-  name    = "${var.component_name}-dev.rdevopsb81.online"
+  name    = "${var.component_name}-dev.tejadevopsb81.icu"
   type    = "A"
   ttl     = "30"
   records = [aws_instance.instance.private_ip]
