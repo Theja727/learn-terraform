ebb73b76b2efcca827dd055cdf479bfbc48e5200
Teja <awstheja727@gmail.com>
2024-12-31 14:56:16 +0530

    Learning terraform - multi-env-code-structure

---
+++ 08-multi-env-code-structure/modules/ec2/main.tf
@@ -0,0 +1,10 @@
+resource "aws_instance" "instance" {
+  ami           = "ami-09c813fb71547fc4f"
+  instance_type = "t2.micro"
+  vpc_security_group_ids = ["sg-0049ef5ba290b1d61"]
+  tags = {
+    Name = "test-${var.env}"
+  }
+}
+
+variable "env" {}
